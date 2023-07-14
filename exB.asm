47e5b069f98bb8ffe7eaeee9e4a41662bf5e999f
Rafael Varago <87142022+rafael-varago@users.noreply.github.com>
2023-07-13 22:59:59 -0300

    Formatando linhas

--- exB.asm
+++ exB.asm
@@ -22,31 +22,31 @@ main:
 
 produtoEscalar:
 	bgt $a3, $zero, calculo  	# Se $a3 == 0, saira do metodo
-  j resultado			          # Se for zero, ira ao metodo resultado
+	j resultado			          # Se for zero, ira ao metodo resultado
     
 calculo:
 	subu $sp,$sp,8			# Abre espaço na pilha            
         
-  lw $t0, 0($a1)			# Carregar o elemento do vetor A
-  lw $t1, 0($a2)       		# Carregar o elemento do vetor B
+  	lw $t0, 0($a1)			# Carregar o elemento do vetor A
+ 	lw $t1, 0($a2)       		# Carregar o elemento do vetor B
 			      
-  mul $t2, $t0, $t1		# Multiplica os elementos
+  	mul $t2, $t0, $t1		# Multiplica os elementos
     	
-  sw $t2,4($sp)			# Armazena o resultado parcial na pilha               
-  sw $ra,0($sp)			# Armazena a posicao do programa
+  	sw $t2,4($sp)			# Armazena o resultado parcial na pilha               
+  	sw $ra,0($sp)			# Armazena a posicao do programa
     
-  addi $a1, $a1, 4		# Avanca para o proximo elemento em A
-  addi $a2, $a2, 4		# Avanca para o proximo elemento em B
+  	addi $a1, $a1, 4		# Avanca para o proximo elemento em A
+  	addi $a2, $a2, 4		# Avanca para o proximo elemento em B
     
-  subu $a3, $a3, 1		# Decrementar o contador
+  	subu $a3, $a3, 1		# Decrementar o contador
     	
-  jal produtoEscalar		# Volta ao produtoEscalar para verificar se ainda ha posicoes para realizar o calculo
+  	jal produtoEscalar		# Volta ao produtoEscalar para verificar se ainda ha posicoes para realizar o calculo
     	
 	lw $ra,0($sp)			# Recupera o valor original da proxima posicao
 	lw $t3,4($sp)			# Recupera o valor original do $t2 em $t3
 	addu $sp,$sp,8			# Retira o espaco ocupado na pilha
     	
-  add $a0, $a0, $t3		# Soma o valor que esta na pilha ao $a0 (resultado)
+  	add $a0, $a0, $t3		# Soma o valor que esta na pilha ao $a0 (resultado)
     	
 resultado:
 	jr $ra				# Volta a posicao do programa salva na pilha
